c4ec201b6dc44746e1ff56da4e15c013c3b68396
Akshat Garg <akshatg805@gmail.com>
2025-05-06 14:35:44 +0530

    revert: decommisioning SSE bcz of Vercel's time limit of 10 secs

--- app/(store)/all-orders/page.jsx
+++ app/(store)/all-orders/page.jsx
@@ -1,12 +1,5 @@
-"use client";
-
-import { parseCookies } from "nookies";
-
 import Store from "@/components/all-orders/store";
 
 export default function Page() {
-  const cookies = parseCookies();
-  const user = cookies.user;
-
-  return <Store user={user} />;
+  return <Store />;
 }
